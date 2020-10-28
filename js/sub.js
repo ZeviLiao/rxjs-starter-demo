442e81e3df64cb49e007bf4ed52ee9c87eabaa7c
Zevi Liao <jointjm@gmail.com>
2020-10-29 04:44:19 +0800

    update

--- js/sub.js
+++ js/sub.js
@@ -1,16 +1,17 @@
 import { defer, Observable, of, Subject } from "rxjs";
 
-const source = defer(() => of(Math.floor(Math.random() * 100)));
-
-const subject = new Subject<number>();
-subject.subscribe(observer("a"));
-subject.subscribe(observer("b"));
-source.subscribe(subject);
-
-
-function observer(name: string) {
+function observer(name) {
     return {
-      next: (value: number) => console.log(`observer ${name}: ${value}`),
-      complete: () => console.log(`observer ${name}: complete`)
+        next: (value) => console.log(`observer ${name}: ${value}`),
+        complete: () => console.log(`observer ${name}: complete`)
     };
-  }
+}
+
+export const rxTest = () => {
+    const source = defer(() => of(Math.floor(Math.random() * 100)));
+
+    const subject = new Subject();
+    subject.subscribe(observer("a"));
+    subject.subscribe(observer("b"));
+    source.subscribe(subject);
+}
