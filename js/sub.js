c257349f3e0f1a44738be91d0f51945e3a16a073
Zevi Liao <jointjm@gmail.com>
2020-10-29 04:25:30 +0800

    add folder

---
+++ js/sub.js
@@ -0,0 +1,16 @@
+import { defer, Observable, of, Subject } from "rxjs";
+
+const source = defer(() => of(Math.floor(Math.random() * 100)));
+
+const subject = new Subject<number>();
+subject.subscribe(observer("a"));
+subject.subscribe(observer("b"));
+source.subscribe(subject);
+
+
+function observer(name: string) {
+    return {
+      next: (value: number) => console.log(`observer ${name}: ${value}`),
+      complete: () => console.log(`observer ${name}: complete`)
+    };
+  }
